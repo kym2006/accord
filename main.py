c11f4732589c3b06ae93a5610ff02f467fffea61
kym2006 <kangyiming2006@gmail.com>
2021-05-29 11:04:18 +0800

    countdown

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import flask 
-from flask import render_template
+from flask import render_template, Response
 from flask.globals import request
+from datetime import datetime
 import math
 import time
 app=flask.Flask(__name__)
+timeleft = -1
 @app.route('/')
 def root():
     return render_template("index.html")
 
+@app.route('/time_feed')
+def time_feed():
+    global timeleft 
+    timeleft = timeleft - 1
+    def generate():
+        if timeleft < 0:
+            return ''
+        
+        return str(timeleft)  # return also will work
+    return Response(generate(), mimetype='text') 
+
+@app.route('/loadtime', methods=['GET', 'POST'])
+def loadtime():
+    global timeleft 
+    if request.method == "POST":
+        timeleft = int(request.form['settime']) * 60 
+    return render_template("countingdown.html")
 
 app.run(host="0.0.0.0", port=8080, debug=True)
 
