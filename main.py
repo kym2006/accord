ff22f425d0f54da218086f8edbb41bb204386aa0
kym2006 <kangyiming2006@gmail.com>
2021-05-30 09:18:08 +0800

    saving to datafile

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from flask.globals import request
 from datetime import datetime
 import math
 import time
+import json 
+import os 
 app=flask.Flask(__name__)
 timeleft = -1
+settime = -1
 @app.route('/')
 def root():
     return render_template("index.html")
@@ -13,6 +16,8 @@ def root():
 @app.route('/done') 
 def done():
     return render_template('index.html', done=1)
+
+
 
 @app.route('/time_feed')
 def time_feed():
@@ -28,10 +33,37 @@ def time_feed():
 
 @app.route('/loadtime', methods=['GET', 'POST'])
 def loadtime():
-    global timeleft 
+    global timeleft, settime
     if request.method == "POST":
         timeleft = int(request.form['settime']) * 60 
+        settime = timeleft//60
     return render_template("countingdown.html")
+
+@app.route('/viewlogs')
+def viewlogs():
+    return render_template("logs.html")
+
+@app.route('/intolog', methods=['GET', 'POST'])
+def intolog():
+    print(request.form['desc'])
+    if os.path.exists("data.json"):
+        with open("data.json") as datafile:
+            savedata = json.load(datafile)
+    else:
+        savedata = {}
+        savedata["id"] = 0
+    
+    id=savedata["id"]
+    savedata["id"]+=1
+    savedata[id] = dict( {
+        "desc": request.form["desc"],
+        "enddate": str(datetime.now()),
+        "duration": settime
+    })
+    with open("data.json", "w+") as datafile:
+        json.dump(savedata, datafile)
+
+    return render_template("index.html")
 
 app.run(host="0.0.0.0", port=8080, debug=True)
 
