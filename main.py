d4dd48767e98067545f8dc262776b26ce1bd328d
Kang Yiming <kangyiming2006@gmail.com>
2021-06-26 19:19:39 +0800

    comments

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = flask.Flask(__name__)
 timeleft = 0
 settime = -1
 
-def getdata():
+def getdata(): # this function loads the data file and initiates the dictionary properly 
     if os.path.exists("data.json"):
         with open("data.json") as datafile:
             savedata = json.load(datafile)
@@ -24,23 +24,23 @@ def getdata():
         savedata["id"] = 0 
     return savedata 
 
-@app.route('/')
+@app.route('/') # root view
 def root():
     return render_template("index.html")
 
-@app.route('/settings')
+@app.route('/settings') # settings page
 def settings():
     savedata = getdata()
     return render_template("settings.html", tags=savedata["tags"])
 
 
-@app.route('/done')
+@app.route('/done') # makes the popup show up in the root view
 def done():
     savedata = getdata()
     return render_template('index.html', done=1, tags=savedata["tags"])
 
 
-@app.route('/addtag', methods=["POST"])
+@app.route('/addtag', methods=["POST"]) # posting to database to add a tag
 def addtag():
     savedata = getdata()
     if request.form["newtag"] not in savedata["tags"]:
@@ -51,13 +51,13 @@ def addtag():
     return render_template("settings.html", tags=savedata["tags"])
 
 
-@app.route('/changepic', methods=["POST"])
+@app.route('/changepic', methods=["POST"]) # changing the picture. The picture will just override the existing background picture
 def changepic():
     savedata = getdata()
     request.files['file1'].save("static/notsogeneric1.jpg")
     return render_template("settings.html", tags=savedata["tags"])
 
-@app.route('/time_feed')
+@app.route('/time_feed') # this function is called once per second and will update the timer 
 def time_feed():
     global timeleft
     timeleft = timeleft - 1
@@ -71,7 +71,7 @@ def time_feed():
     return Response(generate(), mimetype='text')
 
 
-@app.route('/loadtime', methods=['GET', 'POST'])
+@app.route('/loadtime', methods=['GET', 'POST']) # This starts the timer. If the time is 0, then it'll return the root view
 def loadtime():
     global timeleft, settime
     if request.method == "POST":
@@ -84,7 +84,7 @@ def loadtime():
     return render_template("countingdown.html")
 
 
-@app.route('/resettimer', methods=['GET', 'POST'])
+@app.route('/resettimer', methods=['GET', 'POST']) # this resets the timer and redirects to the homepage
 def resettimer():
     global timeleft
     timeleft = 0
@@ -97,10 +97,9 @@ def viewlogs():
     for k, v in savedata.items():
         if type(v) == type(savedata):
             v['enddate'] = datetime.strptime(
-                v['enddate'], '%Y-%m-%d %H:%M:%S.%f')
-    savedata = dict(list(savedata.items())[1:])
+                v['enddate'], '%Y-%m-%d %H:%M:%S.%f') # This saves the datetime in the following format 
+    savedata = dict(list(savedata.items())[1:]) # This discards the id field
     info = dict()
-    print(savedata)
     for k, v in savedata.items():
         if type(v) != type(dict()):
             continue
@@ -112,11 +111,10 @@ def viewlogs():
         di['starttime'] = start.strftime("%H:%M")
         di['endtime'] = v['enddate'].strftime("%H:%M")
         di['duration'] = str(v['duration']//3600).rjust(2, '0') + ":" + str(
-            (v['duration'] % 3600)//60).rjust(2, '0') + ":" + str(v['duration'] % 60).rjust(2, '0')
+            (v['duration'] % 3600)//60).rjust(2, '0') + ":" + str(v['duration'] % 60).rjust(2, '0') # padding right to make it look proper
         di['desc'] = v['desc']
         info[datestr].append(di)
-    print(type(list(info.items())))
-    return render_template("logs.html", logs=reversed(list(info.items())))
+    return render_template("logs.html", logs=reversed(list(info.items()))) # we pass the logs in reverse so the things with a later time will appear later
 
 
 @app.route('/intolog', methods=['GET', 'POST'])
@@ -128,10 +126,8 @@ def intolog():
     savedata["id"] += 1
     desc = request.form["desc"]
     for k, v in request.form.items():
-        print(k[:3])
         if k[:3] == "tag" and v=="on":
-            desc += f"| {k[3:]} "
-    print(desc)
+            desc += f"| {k[3:]} " # the first 3 characters are tag which are useless
     savedata[id] = dict({
         "desc": desc,
         "enddate": str(datetime.now()),
@@ -144,7 +140,6 @@ def intolog():
 
 @app.route('/removetag', methods=['POST'])
 def removetag():
-    print(request.json["value"])
     savedata = getdata()
     savedata["tags"].remove(request.json["value"])
     with open("data.json", "w+") as datafile:
